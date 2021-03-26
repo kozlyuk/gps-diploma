64a21313c0990749c35f25ef9738af4159c4ee46
Andriy Podobailo <47588502+EndL11@users.noreply.github.com>
2021-03-26 15:02:50 +0200

    added simple movement to marker by code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import "./App.css";
 
 function App() {
-  const position = [51.505, -0.09]
+  const pos = [51.505, -0.09];
+  const [position, setPosition] = React.useState(pos);
+  React.useEffect(() => {
+     const update = setInterval(() => {
+      const newPos = [...position];
+      newPos[0] += 0.001;
+      setPosition(newPos);
+    }, 1000);
+    return () => clearInterval(update);
+  }, [position]);
+
+  
+  // function LocationMarker() {
+  //   const [position, setPosition] = React.useState(null)
+  //   const map = useMapEvents({
+  //     click() {
+  //       map.locate()
+  //     },
+  //     locationfound(e) {
+  //       setPosition(e.latlng)
+  //       map.flyTo(e.latlng, map.getZoom())
+  //     },
+  //   })
+  
+  //   return position === null ? null : (
+  //     <Marker position={position}>
+  //       <Popup>You are here</Popup>
+  //     </Marker>
+  //   )
+  // }
   return (
-    <MapContainer center={position} minZoom={8} zoom={10} scrollWheelZoom={false}>
+    <MapContainer center={position} minZoom={8} zoom={10}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -15,6 +44,7 @@ function App() {
           A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
       </Marker>
+      {/* <LocationMarker /> */}
     </MapContainer>
   )
 }
