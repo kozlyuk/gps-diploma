aeddfc0c04d0471f2a5d85ad5706d8881392ae42
Andriy Podobailo <47588502+EndL11@users.noreply.github.com>
2021-06-20 00:42:04 +0300

    added outline for marker

--- src/components/MarkerWrapper.jsx
+++ src/components/MarkerWrapper.jsx
@@ -16,7 +16,10 @@ export const MarkerWrapper = ({ car }) => {
 
   if (car.hasOwnProperty("last_position")) {
     const divider = 10000000;
-    position = [car.last_position.latitude / divider, car.last_position.longitude / divider];
+    position = [
+      car.last_position.latitude / divider,
+      car.last_position.longitude / divider,
+    ];
   }
 
   const markerImageHtmlStyles = `
@@ -25,25 +28,32 @@ export const MarkerWrapper = ({ car }) => {
   transform: rotate(-45deg);
   border-radius: 3rem 3rem 0;`;
 
+  const wrapper = `
+  width: 1.6rem;
+  height: 1.6rem;
+  display: block;
+  left: -0.8rem;
+  top: -0.3rem;
+  position: relative;
+  transform: rotate(45deg);
+  border-radius: 3rem 3rem 0;
+  border: 1px solid #000`;
+
   const markerHtmlStyles = `
     background-color: ${car.color};
     width: 1.5rem;
     height: 1.5rem;
     display: block;
-    left: -0.8rem;
-    top: -0.3rem;
-    position: relative;
     border-radius: 3rem 3rem 0;
-    transform: rotate(45deg);
     border: 1px solid #FFFFFF`;
-    const img = `<img style="${markerImageHtmlStyles}" src="${imgSource}" alt="no image"/>`;
+  const img = `<img style="${markerImageHtmlStyles}" src="${imgSource}" alt="no image"/>`;
 
   const icon = Leaflet.divIcon({
     className: "",
     iconAnchor: [0, 24],
     tooltipAnchor: [11, -11],
     popupAnchor: [0, -11],
-    html: `<span style="${markerHtmlStyles}"></span>`,
+    html: `<span style="${wrapper}"><span style="${markerHtmlStyles}"></span></span>`,
   });
 
   const {
